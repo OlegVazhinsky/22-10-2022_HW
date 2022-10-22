8c6a75f5ae36c98d49e398baed7c2195e4fcc482
OlegVazhinsky <oleg_vazhinsky92@mail.ru>
2022-10-22 16:12:11 +0700

    Выполнил задание 19.

---
+++ Task19/Program.cs
@@ -0,0 +1,24 @@
+﻿int GetNumber (string message) {
+    int userNumber;
+    while (true) {
+        Console.Write(message);
+ 
+        if (int.TryParse(Console.ReadLine(), out int number) && ((number < 100000 && number > 9999) || ((number > -100000 && number < -9999)))) {
+            userNumber = number;
+            break;
+        }    
+        else {
+            Console.WriteLine($"{number} не является пятизначным числом, повторите ввод.");
+        }
+    } 
+    return userNumber;
+}
+
+int userNumber = GetNumber("Введите пятизначное число - ");
+
+if ((userNumber / 10000 == userNumber % 10) && ((userNumber / 1000) % 10 == (userNumber % 100) / 10)) {
+    Console.WriteLine($"Число {userNumber} является палиндромом.");
+}
+else {
+    Console.WriteLine($"Число {userNumber} не является палиндромом.");
+}
